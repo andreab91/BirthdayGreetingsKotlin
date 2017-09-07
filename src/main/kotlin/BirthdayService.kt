c24d70c83282623a38b528b353c8553a4d1c0b6c
Andrea Bergamini <andrea.bergamini@xpeppers.com>
2017-09-07 17:19:24 +0200

    extracts sendMessage to Mailer class

--- src/main/kotlin/BirthdayService.kt
+++ src/main/kotlin/BirthdayService.kt
@@ -1,10 +1,3 @@
-import java.util.*
-import javax.mail.Message
-import javax.mail.Session
-import javax.mail.Transport
-import javax.mail.internet.InternetAddress
-import javax.mail.internet.MimeMessage
-
 open class BirthdayService(private val employeeRepository: EmployeeRepository) {
 
     fun sendGreetings(xDate: XDate, smtpHost: String, smtpPort: Int) {
@@ -19,19 +12,6 @@ open class BirthdayService(private val employeeRepository: EmployeeRepository) {
     }
 
     protected open fun sendMessage(smtpHost: String, smtpPort: Int, sender: String, subject: String, body: String, recipient: String) {
-        val props = Properties()
-        props.put("mail.smtp.host", smtpHost)
-        props.put("mail.smtp.port", smtpPort.toString())
-        val session = Session.getInstance(props, null)
-
-        val msg = MimeMessage(session)
-        msg.setFrom(InternetAddress(sender))
-        msg.setRecipient(Message.RecipientType.TO, InternetAddress(recipient))
-        msg.subject = subject
-        msg.setText(body)
-
-        Transport.send(msg)
+        Mailer(smtpHost, smtpPort).sendMessage(sender, subject, body, recipient)
     }
-
 }
-
---
+++ src/main/kotlin/Mailer.kt
@@ -0,0 +1,23 @@
+import java.util.*
+import javax.mail.Message
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+class Mailer(val smtpHost: String, val smtpPort: Int) {
+    open fun sendMessage(sender: String, subject: String, body: String, recipient: String) {
+        val props = Properties()
+        props.put("mail.smtp.host", smtpHost)
+        props.put("mail.smtp.port", smtpPort.toString())
+        val session = Session.getInstance(props, null)
+
+        val msg = MimeMessage(session)
+        msg.setFrom(InternetAddress(sender))
+        msg.setRecipient(Message.RecipientType.TO, InternetAddress(recipient))
+        msg.subject = subject
+        msg.setText(body)
+
+        Transport.send(msg)
+    }
+}
